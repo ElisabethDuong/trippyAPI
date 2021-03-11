c9ee68d92f6ea07a330e6f5cc9802d59c03be705
Ewa <kalistaewa@gmail.com>
2021-03-11 15:12:36 +0100

    DB created

---
+++ fixtures.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const hotelModel = require("./models/hotels");
+const restaurantModel = require("./models/restaurants");
+
+const hotels = [
+  {
+    name: "Georges V",
+    address: "5 avenue Champs Elysées",
+    city: "Paris",
+    country: "France",
+    stars: 5, //entre 1 et 5
+    hasSpa: true,
+    hasPool: false,
+    priceCategory: 3, // entre 1 et 3
+  },
+  {
+    name: "Sursum Corda",
+    address: "Trasteverde 12",
+    city: "Roma",
+    country: "Italy",
+    stars: 3, //entre 1 et 5
+    hasSpa: false,
+    hasPool: false,
+    priceCategory: 1, // entre 1 et 3
+  },
+
+  {
+    name: "Sirène",
+    address: "34 promenade des Anglais",
+    city: "Nice",
+    country: "France",
+    stars: 4, //entre 1 et 5
+    hasSpa: true,
+    hasPool: true,
+    priceCategory: 2, // entre 1 et 3
+  },
+];
+
+const restaurants = [
+  {
+    name: "Mazurka",
+    address: "18 avenue Opera ",
+    city: "Paris",
+    country: "France",
+    stars: 4, // entre 1 et 3
+    cuisine: "spécialitées franco-polonaises",
+    priceCategory: 3, // entre 1 et 3
+  },
+
+  {
+    name: "Dolce Vita",
+    address: "3 bd Montparnase",
+    city: "Paris",
+    country: "France",
+    stars: 3, // entre 1 et 3
+    cuisine: "cuisine italienne",
+    priceCategory: 2, // entre 1 et 3
+  },
+  {
+    name: "Ichiran",
+    address: "Palais de TOKIO",
+    city: "Tokio",
+    country: "Japan",
+    stars: 3, // entre 1 et 3
+    cuisine: "saveurs japonaises",
+    priceCategory: 2, // entre 1 et 3
+  },
+];
+
+mongoose.connect("mongodb://localhost:27017/trippy-API", () => {
+  console.log("Database Connected");
+});
+
+hotelModel.deleteMany({}).then(() => {
+  hotelModel.create(hotels);
+});
+
+restaurantModel.deleteMany({}).then(() => {
+  restaurantModel.create(restaurants);
+});
--- models/hotels.js
+++ models/hotels.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const hotelSchema = new mongoose.Schema({
-    name: String,
-    address: String,
-    city: String,
-    country: String,
-    stars: Number,
-    hasSpa: Boolean,
-    hasPool: Boolean,
-    priceCategory: Number,
+  name: String,
+  address: String,
+  city: String,
+  country: String,
+  stars: Number, //entre 1 et 5
+  hasSpa: Boolean,
+  hasPool: Boolean,
+  priceCategory: Number, // entre 1 et 3
 });
 const hotelModel = mongoose.model("hotels", hotelSchema);
 module.exports = hotelModel;
