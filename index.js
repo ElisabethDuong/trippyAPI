6d34bbb14e90ac0742953aa2dca1a58fd216efe7
Ewa <kalistaewa@gmail.com>
2021-03-11 15:53:15 +0100

    serveur node works

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const hotelModel = require("./models/hotels");
+const restaurantModel = require("./models/restaurants");
 
 mongoose.connect(
-    "mongodb://localhost:27017/trippy_basics",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {console.log("Database Connected")}
+  "mongodb://localhost:27017/trippy-API",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Database Connected");
+  }
 );
 
+const port = 8000;
+const app = express();
+
+app.listen(port, () => {
+  console.log("serveur works");
+});
