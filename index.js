d8a6c6f5d352fbbd3e75a76778638111f6a1b5bf
Ewa <kalistaewa@gmail.com>
2021-03-15 17:24:17 +0100

    commentaires

--- index.js
+++ index.js
@@ -6,22 +6,23 @@ const { request } = require("express");
 const bodyParser = require("body-parser");
 
 mongoose.connect(
-    "mongodb://localhost:27017/trippy-API",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-        console.log("Database Connected");
-    }
+  "mongodb://localhost:27017/trippy-API",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Database Connected");
+  }
 );
 
 const port = 8000;
 const app = express();
 
 app.listen(port, () => {
-    console.log("serveur works");
+  console.log("serveur works");
 });
 
 app.use(bodyParser.json());
 
+// hotels
 app.get("/hotels", async (request, response) => {
   const hotels = await hotelModel.find().lean().exec();
   response.json(hotels);
@@ -48,8 +49,6 @@ app.put("/hotels/:id", async (request, response) => {
     { _id: request.params.id },
     { name: newName }
   );
-  console.log(hotel);
-  console.log(hotel.name);
   hotel.name;
   response.json(hotel);
   response.send("hotêl mis à jour");
@@ -61,55 +60,55 @@ app.delete("/hotels/:id", async (request, response) => {
   });
   response.json(hotelDeleted);
 });
+
+//restaurants
 app.get("/restaurants", async (request, response) => {
-    const restaurants = await restaurantModel.find().lean().exec();
-    response.json(restaurants);
+  const restaurants = await restaurantModel.find().lean().exec();
+  response.json(restaurants);
 });
 
 app.get("/restaurants/:id", async (request, response) => {
-    const restaurants = await restaurantModel
-        .findOne({
-            _id: request.params.id
-        })
-        .lean()
-        .exec()
-    response.json(restaurants);
+  const restaurants = await restaurantModel
+    .findOne({
+      _id: request.params.id,
+    })
+    .lean()
+    .exec();
+  response.json(restaurants);
 });
 
 app.post("/restaurants", async (request, response) => {
-    await restaurantModel.create({
-        name: request.body.name,
-        address: request.body.address,
-        city: request.body.city,
-        country: request.body.country,
-        stars: request.body.stars,
-        cuisine: request.body.cuisine,
-        priceCategory: request.body.priceCategory
-    });
-    response.send("Restaurant ajouté")
+  await restaurantModel.create({
+    name: request.body.name,
+    address: request.body.address,
+    city: request.body.city,
+    country: request.body.country,
+    stars: request.body.stars,
+    cuisine: request.body.cuisine,
+    priceCategory: request.body.priceCategory,
+  });
+  response.send("Restaurant ajouté");
 });
 
 app.put("/restaurants/:id", async (request, response) => {
-    // console.log(request.query);
-    const newName = request.query.newName;
-    await restaurantModel
-        .updateOne(
-            {
-                _id: request.params.id,
-            },
-            {
-                name: newName
-            },
-        )
-        .exec();
-    response.send("Nom mis à jour")
+  // console.log(request.query);
+  const newName = request.query.newName;
+  await restaurantModel
+    .updateOne(
+      {
+        _id: request.params.id,
+      },
+      {
+        name: newName,
+      }
+    )
+    .exec();
+  response.send("Nom mis à jour");
 });
 
 app.delete("/restaurants/:id", async (request, response) => {
-    await restaurantModel
-        .deleteOne(
-            {
-                _id: request.params.id
-            }),
-        response.send("Restaurant supprimé")
+  await restaurantModel.deleteOne({
+    _id: request.params.id,
+  }),
+    response.send("Restaurant supprimé");
 });
